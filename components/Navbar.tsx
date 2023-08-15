96ce82cc1caecc7ddde151b2137b0c5fd8aacc93
thientantran <thientantm@gmail.com>
2023-08-15 09:03:46 +0700

    Create Sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,9 +13,9 @@ const font = Poppins({
 })
 export default function Navbar() {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu />
+        <Menu className="block md:hidden" />
         <Link href='/'>
           <h1 className={cn("hidden text-xl font-bold text-primary md:block md:text-3xl", font.className)}>
             Companion.ai
