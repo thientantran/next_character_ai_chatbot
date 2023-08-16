9a782a04b59149cf9b2ad4c25f7f2fcc3d995814
thientantran <thientantm@gmail.com>
2023-08-16 11:35:51 +0700

    Category buttons - Query

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,14 +1,31 @@
 'use client'
 import { cn } from "@/lib/utils";
 import { Category } from "@prisma/client";
-
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 interface CategoriesProps {
   data: Category[]
 }
 export default function Categories({ data }: CategoriesProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+
+  const onClick = (id: string | undefined) => {
+    const query = { categoryId: id };
+
+    const url = qs.stringifyUrl({
+      url: window.location.href,
+      query
+    }, { skipNull: true });
+
+    router.push(url);
+  };
   return (
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
       <button
+        onClick={() => onClick(undefined)}
         className={cn(`
           flex 
           items-center 
@@ -23,13 +40,15 @@ export default function Categories({ data }: CategoriesProps) {
           bg-primary/10 
           hover:opacity-75 
           transition
-        `
+        `,
+          !categoryId ? 'bg-primary/25' : 'bg-primary/10'
         )}
       >
         Newest
       </button>
       {data.map((item) => (
         <button
+          onClick={() => onClick(item.id)}
           className={cn(`
             flex 
             items-center 
@@ -44,7 +63,8 @@ export default function Categories({ data }: CategoriesProps) {
             bg-primary/10 
             hover:opacity-75 
             transition
-          `
+          `,
+            item.id === categoryId ? 'bg-primary/25' : 'bg-primary/10'
           )}
           key={item.id}
         >
