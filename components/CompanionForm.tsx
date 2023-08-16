b34a4fec39b3992ac9573d1d1fbfdb3bea70c820
thientantran <thientantm@gmail.com>
2023-08-16 21:35:24 +0700

    set up react hook form for form component
    create form SChema using zod
    create form using UseForm and add resolver, default values

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -1,5 +1,29 @@
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Category, Companion } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required.",
+  }),
+  description: z.string().min(1, {
+    message: "Description is required.",
+  }),
+  instructions: z.string().min(200, {
+    message: "Instructions require at least 200 characters."
+  }),
+  seed: z.string().min(200, {
+    message: "Seed requires at least 200 characters."
+  }),
+  src: z.string().min(1, {
+    message: "Image is required."
+  }),
+  categoryId: z.string().min(1, {
+    message: "Category is required",
+  }),
+});
 interface CompanionFormProps {
   categories: Category[];
   initialData: Companion | null;
@@ -8,6 +32,22 @@ export default function CompanionForm({
   categories,
   initialData
 }: CompanionFormProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: "",
+      description: "",
+      instructions: "",
+      seed: "",
+      src: "",
+      categoryId: undefined,
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
   return (
     <div>CompanionForm</div>
   )
