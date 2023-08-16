02225dd82c9a94dd710f2f6d717d0a9349708fc9
thientantran <thientantm@gmail.com>
2023-08-16 10:58:53 +0700

    Binding Category into APP

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,13 @@
-import SearchInput from "@/components/SearchInput"
-export default function RootPage() {
+import Categories from "@/components/Categories";
+import SearchInput from "@/components/SearchInput";
+import prismadb from "@/lib/prismadb";
+export default async function RootPage() {
+  const categories = await prismadb.category.findMany();
   return (
     <div>
       <div className="f-full p-4 space-y-2">
         <SearchInput />
+        <Categories data={categories} />
       </div>
     </div>
   )
