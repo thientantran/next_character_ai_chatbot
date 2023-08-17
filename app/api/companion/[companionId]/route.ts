66af5c87f8b273eb3246dedb4ab6c9a327d58521
thientantran <thientantm@gmail.com>
2023-08-17 17:29:56 +0700

    Edit API for companion

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -2,19 +2,27 @@ import { currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
-export async function POST(req: Request) {
+// add them parameter
+export async function PATCH(req: Request, { params }: { params: { companionId: string } }) {
   try {
     const body = await req.json()
     const user = await currentUser()
     const { src, name, description, instructions, seed, categoryId } = body;
+    // add them dieu kien
+    if (!params.companionId) {
+      return new NextResponse("Companion ID is required", { status: 400 })
+    }
     if (!user || !user.id || !user.firstName) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
     if (!src || !name || !description || !instructions || !seed || !categoryId) {
       return new NextResponse("Missing required fields", { status: 400 })
     }
-
-    const componion = await prismadb.companion.create({
+    //change create -> update
+    const componion = await prismadb.companion.update({
+      where: {
+        id: params.companionId
+      },
       data: {
         categoryId,
         userId: user.id,
@@ -29,7 +37,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json(componion)
   } catch (error) {
-    console.log("[COMPANION_POST]", error)
+    console.log("[COMPANION_PATCH]", error)
     return new NextResponse("Internal Error", { status: 500 })
   }
 }
